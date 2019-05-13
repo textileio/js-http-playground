5e575563d0606908aac205b0be91b044f9cd3168
andrew <andrewxhill@gmail.com>
2019-05-13 10:35:30 -0700

    simplify the profile example
    Signed-off-by: andrew <andrewxhill@gmail.com>

--- recipes/Profile/index.test.ts
+++ recipes/Profile/index.test.ts
@@ -11,30 +11,16 @@ import textile, { Peer } from '@textile/js-http-client'
 jest.setTimeout(25000)
 
 describe('Profile API Examples', () => {
-  it('Get your profile, change your name, revert it back to current name', async (done) => {
-    let profile: Peer
-    let name: string
-    let success: boolean
+  it('Get your profile and display name', async (done) => {
 
     // Simply get your display name
-    name = await textile.profile.name()
+    const name = await textile.profile.name()
     console.info('Your are connected:', name)
 
     // Get your full profile record
-    profile = await textile.profile.get()
+    const profile = await textile.profile.get()
     console.info('Your ID is:', profile.id)
 
-    // Set your display name to 'Anonymous'
-    success = await textile.profile.setName('Anonymous')
-    const updatedProfile = await textile.profile.get()
-    console.info('Profile name updated to:', updatedProfile.name)
-
-    success = await textile.profile.setName(name)
-    console.info('Your name is now reverted to:', name)
-
-    // Jest Tests to ensure your account reverted successfully
-    await expect(success).toBeTruthy()
-    await expect((await textile.profile.get()).name).toEqual(name)
     done()
   })
 })
