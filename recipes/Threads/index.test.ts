a661e862f552f7764d76d951521ca6de09479fe5
andrew <andrewxhill@gmail.com>
2019-05-17 11:11:46 -0700

    Adds a couple new recipes
    Signed-off-by: andrew <andrewxhill@gmail.com>

--- recipes/Threads/index.test.ts
+++ recipes/Threads/index.test.ts
@@ -1,5 +1,5 @@
 import textile from '@textile/js-http-client'
-import * as slateSchema from '../data/slatejs.json'
+import slateSchema from '../data/slatejs.json'
 import { createLocationDataThread, locationDataThreadKey, blobThreadKey, removeBlobThread, removeLocationDataThread, slateNotesThreadKey } from '../Threads/index'
 
 /**
@@ -42,6 +42,39 @@ describe('Threads API Recipes', () => {
     console.info('Custom JSON thread success!')
     done()
   })
+
+  it('Create a notes thread with a Slate.js JSON Document schema', async (done) => {
+    const existing = await textile.threads.getByKey(slateNotesThreadKey)
+    if (!existing) {
+      const thread = await textile.threads.add('My Notes', slateSchema, slateNotesThreadKey, 'private', 'invite_only')
+      console.info('Notes thread created successfully')
+      await textile.threads.remove(thread.id)
+    } else {
+      console.info('Notes thread already exists. Recipe not run.')
+      await textile.threads.remove(existing.id)
+    }
+    done()
+  })
+
+  it('Rename a thread', async (done) => {
+    await createLocationDataThread()
+    // See the createLocationDataThread example in index.ts
+    const thread = await textile.threads.getByKey(locationDataThreadKey)
+    if (!thread) {
+      console.info('Example locations thread does not exist')
+      return done()
+    }
+    await textile.threads.rename(thread.id, 'XYZ')
+    const updated = await textile.threads.getByKey(locationDataThreadKey)
+    if (!updated) {
+      console.info('Something went wrong in rename recipe')
+      return done()
+    }
+    console.info('Updated location thread name to:', updated.name)
+    console.info('Custom JSON thread success!')
+    done()
+  })
+})
 
 afterAll(async (done) => {
   // Removing the Threads used in the Files API examples
